ba2c24f215721c7d207505db914e56b152abd87b
Robert Emerson <roe2pj@virginia.edu>
2014-03-02 21:54:06 -0500

    Loation data ocation data works now, so it just needs to be put into a DB table, and returned to user

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <?php
 require 'flight/Flight.php';
 
-function new_game($lat, $lon) {
-
+function get_location($id) {
     # From http://www.flickr.com/services/api/response.php.html
     $params = array(
-	'api_key'	=> 'cb103a2b4f19e0feddc77c2f6295885c',
-	'method'	=> 'flickr.photos.search',
-	'lat'		=> $lat,
-	'lon'		=> $lon,
-	'format'	=> 'json',
-	'nojsoncallback'=> '1'
+        'api_key'       => 'cb103a2b4f19e0feddc77c2f6295885c',
+        'method'        => 'flickr.photos.geo.getLocation',
+        'photo_id'	=> $id,
+        'format'        => 'json',
+        'nojsoncallback'=> '1'
     );
     $encoded_params = array();
 
     foreach ($params as $k => $v){
 
-	$encoded_params[] = urlencode($k).'='.urlencode($v);
-    }   
+        $encoded_params[] = urlencode($k).'='.urlencode($v);
+    }
 
 
     #
@@ -26,10 +24,44 @@ function new_game($lat, $lon) {
 
     $url = "https://api.flickr.com/services/rest/?".implode('&', $encoded_params);
 
-    echo "<a href=\"$url\"> $url </a> <br><br>";
     $flickr = file_get_contents($url);
     # flickr is dumb and doesn't return "proper" JSON so we have to replace \' with '
-    $flickrProper = str_replace("\\'", "'", $flickr); 
+    $flickrProper = str_replace("\\'", "'", $flickr);
+    $picture = json_decode($flickrProper, true);
+
+    $retval = array();
+    $retval['lat'] = $picture['photo']['location']['latitude'];
+    $retval['lon'] = $picture['photo']['location']['longitude'];
+    return $retval;    
+}
+
+function get_pictures($lat, $lon) {
+    # From http://www.flickr.com/services/api/response.php.html
+    $params = array(
+        'api_key'       => 'cb103a2b4f19e0feddc77c2f6295885c',
+        'method'        => 'flickr.photos.search',
+        'lat'           => $lat,
+        'lon'           => $lon,
+        'format'        => 'json',
+        'nojsoncallback'=> '1'
+    );
+    $encoded_params = array();
+
+    foreach ($params as $k => $v){
+
+        $encoded_params[] = urlencode($k).'='.urlencode($v);
+    }
+
+
+    #
+    # call the API and decode the response
+    #
+
+    $url = "https://api.flickr.com/services/rest/?".implode('&', $encoded_params);
+
+    $flickr = file_get_contents($url);
+    # flickr is dumb and doesn't return "proper" JSON so we have to replace \' with '
+    $flickrProper = str_replace("\\'", "'", $flickr);
     $pictures = json_decode($flickrProper, true);
 
     $picture_list = $pictures['photos']['photo']; //flickr adds a bit extra so we need to trim the returned result
@@ -39,18 +71,28 @@ function new_game($lat, $lon) {
     # Choose 10 locations from everything flickr returned
     # Fails if flickr call didn't work
     if ($pictures['stat'] == 'fail') {
-	echo 'well, that failed';
+        echo 'well, that failed';
     } else {
-    	foreach (range(0, 10) as $number) { //choosing 10 pictures now.
-	    $rand = rand(0, count($picture_list)); 
-	    echo "$rand: ";
-	    $chosenPictures[] = $picture_list[$rand]; 
-	}
+        foreach (range(0, 10) as $number) { //choosing 10 pictures now.
+            $rand = rand(0, count($picture_list)-1);
+            $chosenPictures[] = $picture_list[$rand];
+
+            $picture_list[$rand]['location'] = get_location($picture_list[$rand]['id']);
+        }
     }
+    return $chosenPictures;
+}
+
+function new_game($lat, $lon) {
+
+    # First get the pictures we're using for locations
+    $pictures = get_pictures($lat, $lon);
+
+    # now we've got our pictures, so let's put them in the database
 }
 
 Flight::route('/new_game/@lat/@lon', function($lat, $lon) {
-    echo "lat: $lat <br> lon: $lon <br>";
+
     new_game($lat, $lon);
 });
 
