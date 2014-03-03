b1fac96a54cb3b1f230e3b3e04428544f2014c7c
Robert Emerson <roe2pj@virginia.edu>
2014-03-03 09:43:43 -0500

    Got data to the point where it can be inserted in the database.

--- index.php
+++ index.php
@@ -30,8 +30,10 @@ function get_location($id) {
     $picture = json_decode($flickrProper, true);
 
     $retval = array();
+    
     $retval['lat'] = $picture['photo']['location']['latitude'];
     $retval['lon'] = $picture['photo']['location']['longitude'];
+
     return $retval;    
 }
 
@@ -62,6 +64,7 @@ function get_pictures($lat, $lon) {
     $flickr = file_get_contents($url);
     # flickr is dumb and doesn't return "proper" JSON so we have to replace \' with '
     $flickrProper = str_replace("\\'", "'", $flickr);
+    # Then we have to decode the JSON into a PHP array
     $pictures = json_decode($flickrProper, true);
 
     $picture_list = $pictures['photos']['photo']; //flickr adds a bit extra so we need to trim the returned result
@@ -75,12 +78,31 @@ function get_pictures($lat, $lon) {
     } else {
         foreach (range(0, 10) as $number) { //choosing 10 pictures now.
             $rand = rand(0, count($picture_list)-1);
-            $chosenPictures[] = $picture_list[$rand];
 
+	    # adds location data to each chosen image
             $picture_list[$rand]['location'] = get_location($picture_list[$rand]['id']);
-        }
+	    # adds the randomly selected image to our array of chosen images
+            $chosenPictures[] = $picture_list[$rand];
+	}
     }
+
     return $chosenPictures;
+}
+
+function insert_game($pictures, $lat, $lon) {
+    $gameNum = 0; # Will actually get this value from querying count (SELECT * FROM games) + 1, so we get an id for the next game
+    foreach ($pictures as $key => $value) {
+	$id = $value['id'];
+	$pictureLat = $value['location']['lat'];
+	$picutreLon = $value['location']['lon'];
+	$url = "http://farm".$value['farm'].".staticflickr.com/".$value['server']."/".$id."_".$value['secret'].".jpg";
+
+	# insert all this stuff into the picture table (id, lat, lon, url, gameNum)
+    }
+
+    # once we insert the pictures, we insert a game into the games table
+    # INSERT INTO games (game_id, lat, lon, winner) VALUES (gameNum, lat, lon, NULL)
+    # Date can probably be automagically added when it is inserted
 }
 
 function new_game($lat, $lon) {
@@ -89,6 +111,8 @@ function new_game($lat, $lon) {
     $pictures = get_pictures($lat, $lon);
 
     # now we've got our pictures, so let's put them in the database
+    # this should probably return the data that we added, in some manner our client will use
+    insert_game($pictures, $lat, $lon);
 }
 
 Flight::route('/new_game/@lat/@lon', function($lat, $lon) {
